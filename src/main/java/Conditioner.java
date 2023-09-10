0b96a9be1b4746bf9fa8c901c6ef912a048f6f23
Anna Kochergina <akochergina92@gmail.com>
2023-09-10 14:23:52 +0300

    Get and Set

--- src/main/java/Conditioner.java
+++ src/main/java/Conditioner.java
@@ -2,8 +2,29 @@ public class Conditioner {
 
     public int currentTemperature; //поле с текущей температурой
 
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    //подходит ли это значение прежде чем его присваивать
+    public void setCurrentTemperature(int newCurrentTemperature) {
+        if (newCurrentTemperature < 0) { //если температура меньше нуля то мы не можем ее поставить
+            return;
+        }
+        if (newCurrentTemperature > 30) { //новая температура больше максиамальной уходим из метода
+            return;
+        }
+        currentTemperature = newCurrentTemperature;
+    }
+
+    //увеличение значения кондиционера в полтора раза
+    public void increaseTemp50p() {
+        int target = currentTemperature * 3 / 2; //currentTemperature - текущая температура
+        setCurrentTemperature(target); //вызываем готовый сетер, для проверки превышения максимально допустимого значения
+    }
+
     public void setToMaxTemperature() {    //функция не будет ни чег возвращать, кондиционер просто меняет внутри температуру и не сообщает нам ни чего
 //кондиционер знает какая у него текущая температруа и какая максимальная
-    currentTemperature = 30;
+        currentTemperature = 30;
     }
 }
--- src/test/java/ConditionerTest.java
+++ src/test/java/ConditionerTest.java
@@ -8,10 +8,10 @@ public class ConditionerTest {
     public void shouldSetTemperature() {
         Conditioner cond = new Conditioner();
 
-        cond.currentTemperature = 15; //тестируемое действие поля с температурой
+        cond.setCurrentTemperature(15);  //тестируемое действие поля с температурой
 
         int expected = 15; //ожидаемый
-        int actual = cond.currentTemperature; //фактический
+        int actual = cond.getCurrentTemperature(); //фактический
 
         Assertions.assertEquals(expected, actual);
     }
@@ -24,8 +24,37 @@ public class ConditionerTest {
         cond.setToMaxTemperature(); //тестируемое действие
 
         int expected = 30; //ожидаемый
-        int actual = cond.currentTemperature; //фактический, спрашиваем у кондиционера какая у него текущая температура
+        int actual = cond.getCurrentTemperature(); //фактический, спрашиваем у кондиционера какая у него текущая температура
 
         Assertions.assertEquals(expected, actual);
     }
+
+    //просим установить его 50 градусов
+    @Test
+    public void shouldNotSetTemperatureAboveMax() {
+        Conditioner cond = new Conditioner();
+
+        cond.setCurrentTemperature(50);
+
+        int expected = 0;
+        int actual = cond.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    //увеличение значения кондиционера в полтора раза
+    @Test
+    public void shouldIncreaseTemperature50p() {
+        Conditioner cond = new Conditioner();
+        cond.setCurrentTemperature(25); //задаем кондиционеру выставь температуру на 25 градусов
+
+        cond.increaseTemp50p(); //тестируемое действие
+
+        int expected = 25; // 25 потому что кондиционер не будет увеличивать температру в полтора раза, так как она превышает максимально допустимую
+        int actual = cond.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
